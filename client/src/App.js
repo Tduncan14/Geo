6ea4e9a9913f3200d9e2b0fc72d3d96b85ad6bf1
Treek <nbatre1@yahoo.com>
2019-11-16 14:07:49 -0600

    keep moving forward

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import {WebSocketLink} from 'apollo-link-ws';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 
 const wsLink = new WebSocketLink({
-    uri:'ws://localhost:4000/graphql',
+    uri:'wss://localmeetup.herokuapp.com/graphql',
     options:{
         reconnect:true
     }
